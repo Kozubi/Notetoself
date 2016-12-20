b40db7b2be2eedc332c9c6b14246bf7a45d30aca
Kozubi <marcink00@gmail.com>
2016-12-20 10:56:47 +0100

    + DialogNewNote (not completed)

---
+++ app/src/main/java/com/example/marcin/notetoself/DialogNewNote.java
@@ -0,0 +1,44 @@
+package com.example.marcin.notetoself;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+
+/**
+ * Created by marcin on 20.12.2016.
+ */
+
+public class DialogNewNote extends DialogFragment {
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState)
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        View dialogView = inflater.inflate(R.layout.dialog_new_note, null);
+
+        final EditText editTitle = (EditText) dialogView.findViewById(R.id.editTitle);
+        final EditText editDescription = (EditText) dialogView.findViewById(R.id.editDescription);
+        final CheckBox checkBoxIdea = (CheckBox) dialogView.findViewById(R.id.checkBoxIdea);
+        final CheckBox checkBoxTodo = (CheckBox) dialogView.findViewById(R.id.CheckBoxTodo);
+        final CheckBox checkBoxImportant = (CheckBox) dialogView.findViewById(R.id.checkBoxImportant);
+        Button btnCancel = (Button) dialogView.findViewById(R.id.btnCancel);
+        Button ntnOK = (Button) dialogView.findViewById(R.id.btnOK);
+
+        builder.setView(dialogView).setMessage("Add a new note");
+
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dismiss();
+            }
+        });
+    }
+
+}
