c8cb0bdafc003ab0f85f383746bc3faf3e3e5f65
Kozubi <marcink00@gmail.com>
2016-12-20 18:24:59 +0100

    + Next updates

--- app/src/main/java/com/example/marcin/notetoself/MainActivity.java
+++ app/src/main/java/com/example/marcin/notetoself/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.marcin.notetoself;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     Note mTempNote = new Note();
@@ -10,7 +12,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                DialogShowNote dialog = new DialogShowNote();
+                dialog.sendNoteSelected(mTempNote);
 
+                dialog.show(getFragmentManager(), "123");
+            }
+        });
     }
 
     public void createNewNote(Note n)
